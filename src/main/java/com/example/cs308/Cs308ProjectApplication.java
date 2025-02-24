3b8322884eaebee4adbdc61feaf6a0a8de5cbf1b
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2025-02-24 22:30:42 +0100

    first commit

---
+++ src/main/java/com/example/cs308/Cs308ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.cs308;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs308ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs308ProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cs308/Cs308ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cs308;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs308ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
