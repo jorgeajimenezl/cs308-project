c804baa544e1c2243fc82dafaf52fe9877ebb7eb
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2025-02-27 09:15:15 +0100

    Add debug statements for bean creation in AppConfig

--- src/main/java/com/example/cs308/spring/AppConfig.java
+++ src/main/java/com/example/cs308/spring/AppConfig.java
@@ -9,26 +9,31 @@ import org.springframework.context.annotation.Configuration;
 public class AppConfig {
     @Bean
     public Book book1() {
+        System.out.println("Creating book1 bean");
         return new Book("Harry Potter and Soccers stone", "J. K. Rowling");
     }
 
     @Bean
     public Book book2() {
+        System.out.println("Creating book2 bean");
         return new Book("Harry Potter and the Chamber of Secrets", "J. K. Rowling");
     }
 
     @Bean
     public Book book3() {
+        System.out.println("Creating book3 bean");
         return new Book("Harry Potter and the Prisoner of Azkaban", "J. K. Rowling");
     }
 
     @Bean
     public Review review1() {
+        System.out.println("Creating review1 bean");
         return new Review(5, "Excellent book, I enjoyed it a lot.");
     }
 
     @Bean
     public Review review2() {
+        System.out.println("Creating review2 bean");
         return new Review(4, "A good read, I liked it.");
     }
 
@@ -36,6 +41,7 @@ public class AppConfig {
     public DataInitializer dataInitializer(
             BookCatalog bookCatalog,
             ReviewManager reviewManager) {
+        System.out.println("Creating dataInitializer bean");
         DataInitializer initializer = new DataInitializer(bookCatalog, reviewManager);
         initializer.initData(book1(), book2(), book3(), review1(), review2());
         return initializer;
