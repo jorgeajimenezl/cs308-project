eb2c8c007e66b35c86700b7c6f66e3c547b93107
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2025-02-27 08:56:50 +0100

    Replace Spring Boot dependencies with Spring framework and JUnit 5

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
 	java
-	id("org.springframework.boot") version "3.4.3"
-	id("io.spring.dependency-management") version "1.1.7"
+	id("io.freefair.lombok") version "8.12.2"
+	// id("org.springframework.boot") version "3.4.3"
+	// id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "com.example"
@@ -18,9 +19,12 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	// implementation("org.springframework.boot:spring-boot-starter")
+	// testImplementation("org.springframework.boot:spring-boot-starter-test")
+	// testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	implementation("org.springframework:spring-context:6.1.6")
+	testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.withType<Test> {
