823670ce7d8445380176f59ec7c68b60caf4f865
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2025-03-01 22:24:07 +0100

    Update build.gradle.kts to enable Spring Boot and dependency management

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,7 @@
 plugins {
 	java
-	id("io.freefair.lombok") version "8.12.2"
-	// id("org.springframework.boot") version "3.4.3"
-	// id("io.spring.dependency-management") version "1.1.7"
+	id("org.springframework.boot") version "3.4.3"
+	id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "com.example"
@@ -14,17 +13,23 @@ java {
 	}
 }
 
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
 repositories {
 	mavenCentral()
 }
 
 dependencies {
-	// implementation("org.springframework.boot:spring-boot-starter")
-	// testImplementation("org.springframework.boot:spring-boot-starter-test")
-	// testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-	implementation("org.springframework:spring-context:6.1.6")
-	testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.withType<Test> {
