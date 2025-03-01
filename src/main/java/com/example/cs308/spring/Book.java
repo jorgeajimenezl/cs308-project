2120071742c2558ba29667297b11ccfafbfc93a4
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2025-03-01 22:23:39 +0100

    Remove unused Spring configuration and model classes for book and review management

--- src/main/java/com/example/cs308/spring/Book.java
+++
@@ -1,9 +0,0 @@
-package com.example.cs308.spring;
-
-import lombok.RequiredArgsConstructor;
-
-@RequiredArgsConstructor
-public class Book {
-    private final String title;
-    private final String author;
-}
