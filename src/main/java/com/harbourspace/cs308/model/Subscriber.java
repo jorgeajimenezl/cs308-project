565a06ee2049178a739978fb223c6bab13c48def
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2025-03-04 08:56:39 +0100

    Refactor Subscriber model to use Lombok annotations for cleaner code and remove boilerplate constructors and getters/setters

--- src/main/java/com/harbourspace/cs308/model/Subscriber.java
+++ src/main/java/com/harbourspace/cs308/model/Subscriber.java
@@ -3,31 +3,20 @@ package com.harbourspace.cs308.model;
 import java.time.LocalDateTime;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Subscriber {
     @NotBlank(message = "Email is required")
     @Email(message = "Invalid email format")
-    @Getter
-    @Setter
     private String email;
 
-    @Getter
-    @Setter
     private LocalDateTime subscriptionTime;
-
-    @Getter
-    @Setter
     private String ipAddress;
-
-    public Subscriber() {
-    }
-
-    public Subscriber(String email, LocalDateTime subscriptionTime, String ipAddress) {
-        this.email = email;
-        this.subscriptionTime = subscriptionTime;
-        this.ipAddress = ipAddress;
-    }
 }
