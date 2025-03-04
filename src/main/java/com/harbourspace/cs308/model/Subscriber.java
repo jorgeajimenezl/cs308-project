e8c7e69a2c78d8c176891829a80ea58a05d71daf
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2025-03-04 09:21:29 +0100

    Remove unused Lombok annotations from Subscriber model for cleaner code

--- src/main/java/com/harbourspace/cs308/model/Subscriber.java
+++ src/main/java/com/harbourspace/cs308/model/Subscriber.java
@@ -5,9 +5,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Data
 @NoArgsConstructor
