308b2c114b5da87a784ad8e0d35d23a5879630cb
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2025-03-04 18:16:41 +0100

    feat: Enhance Poll model with JPA annotations and extend BaseEntity for improved data management

--- src/main/java/com/harbourspace/cs308/model/Poll.java
+++ src/main/java/com/harbourspace/cs308/model/Poll.java
@@ -1,13 +1,34 @@
 package com.harbourspace.cs308.model;
 
-import lombok.AllArgsConstructor;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.EqualsAndHashCode;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class Poll {
-    private String pollId;
+@EqualsAndHashCode(callSuper=false)
+@Entity
+public class Poll extends BaseEntity {
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
     private String question;
+
+    @Column(nullable = false)
+    private Boolean multipleChoice = false;
+
+    @Column(nullable = false)
+    private Boolean isPublic = false;
+
+    @Column(unique = true, nullable = true)
+    private String slug;
+
+    @OneToMany(mappedBy = "poll")
+    private List<PollOption> options;
 }
