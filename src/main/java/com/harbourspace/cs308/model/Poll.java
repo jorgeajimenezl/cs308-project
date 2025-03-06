0a1f2fd832f4f26c1cfcbffa650c198f69f05ed6
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2025-03-06 11:25:52 +0100

    refactor: Remove unused imports from EmailSubscriber, Poll, and PollOption classes

--- src/main/java/com/harbourspace/cs308/model/Poll.java
+++ src/main/java/com/harbourspace/cs308/model/Poll.java
@@ -1,14 +1,8 @@
 package com.harbourspace.cs308.model;
 
 import java.util.List;
-
-import com.fasterxml.jackson.annotation.JsonView;
-import com.harbourspace.cs308.rest.Views;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -16,7 +10,6 @@ import lombok.EqualsAndHashCode;
 @Entity
 @Data
 @EqualsAndHashCode(callSuper=false)
-@JsonView(Views.Public.class)
 public class Poll extends BaseEntity {
     @Column(nullable = false)
     private String question;
