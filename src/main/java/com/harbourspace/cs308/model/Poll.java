ea9e1a269fef56e373f2025f56c5270af55e4333
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2025-03-07 18:01:37 +0100

    feat: Add database index on slug column in Poll entity

--- src/main/java/com/harbourspace/cs308/model/Poll.java
+++ src/main/java/com/harbourspace/cs308/model/Poll.java
@@ -3,7 +3,9 @@ package com.harbourspace.cs308.model;
 import java.util.List;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Index;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -14,6 +16,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(callSuper=false)
+@Table(indexes = @Index(columnList = "slug"))
 public class Poll extends BaseEntity {
     @Column(nullable = false)
     private String question;
