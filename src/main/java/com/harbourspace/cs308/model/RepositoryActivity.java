76732cd5874caf7b9f19c4824870179838acf169
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2025-03-13 11:23:30 +0100

    refactor: Refactor RepositoryActivity model to include ID and update activity types

--- src/main/java/com/harbourspace/cs308/model/RepositoryActivity.java
+++ src/main/java/com/harbourspace/cs308/model/RepositoryActivity.java
@@ -8,6 +8,8 @@ import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
@@ -20,24 +22,17 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @EqualsAndHashCode(callSuper=false)
 @Entity
-public class RepositoryActivity extends BaseEntity {
-    @Embeddable
-    public static class Author {
-        private String name;
-        private String email;
-    }
+public class RepositoryActivity {
+    @Id
+    @GeneratedValue
+    private Long id;
 
     @Enumerated(EnumType.STRING)
     private RepositoryActivityType type;
-
-    @Column(nullable = false)
-    @Embedded
-    private Author author;
-    
-    @Column(nullable = false)
-    private String githubUrl;
     
     private String description;
+
+    @Column(nullable = false)
     private Instant timestamp;
 
     @ManyToOne
