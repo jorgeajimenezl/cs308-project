483181cd9ed158470ff4841bfeb55c2ef16a7c9e
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2025-03-13 11:29:44 +0100

    feat: Add GithubService to track repositories and update application properties for schema management

--- src/main/java/com/harbourspace/cs308/service/GithubService.java
+++ src/main/java/com/harbourspace/cs308/service/GithubService.java
@@ -1,5 +1,7 @@
 package com.harbourspace.cs308.service;
 
+import java.time.Instant;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -7,12 +9,18 @@ import com.harbourspace.cs308.model.TrackedRepository;
 import com.harbourspace.cs308.repository.TrackedRepositoryRepository;
 
 @Service
-public class GithubTrackerService {
+public class GithubService {
     @Autowired
     private TrackedRepositoryRepository trackedRepository;
 
     public void trackRepository(String owner, String name) {
-        TrackedRepository repo = new TrackedRepository(owner, name, null, null);
+        TrackedRepository repo = new TrackedRepository(
+            owner, 
+            name, 
+            true, 
+            Instant.EPOCH, 
+            null
+        );
         trackedRepository.save(repo);
     }
 
