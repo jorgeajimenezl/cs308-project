743190a4f1431d85363230423cd2f17e4f8951b7
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2025-03-13 16:18:13 +0100

    feat: Add method to find RepositoryActivity by repositoryId and timestamp

--- src/main/java/com/harbourspace/cs308/repository/RepositoryActivityRepository.java
+++ src/main/java/com/harbourspace/cs308/repository/RepositoryActivityRepository.java
@@ -1,7 +1,13 @@
 package com.harbourspace.cs308.repository;
 
+import java.time.Instant;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.harbourspace.cs308.model.RepositoryActivity;
 
-public interface RepositoryActivityRepository extends JpaRepository<RepositoryActivity, Long> {}
+public interface RepositoryActivityRepository extends JpaRepository<RepositoryActivity, Long> {
+    Page<RepositoryActivity> findByRepositoryIdAndTimestampAfter(Long repositoryId, Instant timestamp, Pageable pageable);
+}
