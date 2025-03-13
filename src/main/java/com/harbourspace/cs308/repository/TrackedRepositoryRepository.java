99190afcd45f00f405ba88f6a79914cbb68429bc
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2025-03-13 11:29:06 +0100

    feat: Add pagination support for active tracked repositories in repository interface

--- src/main/java/com/harbourspace/cs308/repository/TrackedRepositoryRepository.java
+++ src/main/java/com/harbourspace/cs308/repository/TrackedRepositoryRepository.java
@@ -1,7 +1,13 @@
 package com.harbourspace.cs308.repository;
 
+import java.time.Instant;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import com.harbourspace.cs308.model.TrackedRepository;
 
-public interface TrackedRepositoryRepository extends JpaRepository<TrackedRepository, Long> { }
+public interface TrackedRepositoryRepository extends JpaRepository<TrackedRepository, Long> {
+    Page<TrackedRepository> findByActiveTrue(Pageable pageable);
+}
